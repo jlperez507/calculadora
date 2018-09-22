0931cc3b9a689878af3da606f5327d0f6328d3f8
jlperez507 <jose_perezpimentel@hotmail.com>
2018-09-22 12:26:36 -0500

    Calculadora 1.0.0.02

--- js/app.js
+++ js/app.js
@@ -34,7 +34,14 @@ function escibir(te){
     if (document.getElementById('display').innerText == "0"){
         document.getElementById('display').innerText = te;
     }else{
-        document.getElementById('display').innerText = document.getElementById('display').innerText + te;
+      if (igual == true){
+            total = 0.00;
+            operacion = "";
+            igual = false;
+            document.getElementById('display').innerText =  te;
+      }else {
+              document.getElementById('display').innerText = document.getElementById('display').innerText + te;
+      }
     }
   }
 }
@@ -42,33 +49,108 @@ function escibir(te){
 document.getElementById('on').addEventListener('click', function(){
   document.getElementById('display').innerText = '0';
   total = 0;
+  igual = false;
+   operacion = "";
 });
 
 var total = 0;
 var calculo;
 var calculonew;
 var operacion = "";
+var igual = false;
 document.getElementById('igual').addEventListener('click', function(){
   if (operacion != ""){
     if (document.getElementById('display').innerText != ""){
       if (operacion == "+"){
           if (calculonew == ""){
             calculonew  = document.getElementById('display').innerText;
-            total = total +  Number(calculo) + Number(calculonew);
+            total = Number(calculo) + Number(calculonew);
             document.getElementById('display').innerText = total;
           }else {
             total = total +  Number(calculonew);
             document.getElementById('display').innerText = total;
           }
+          igual = true;
+      }else if (operacion == "-") {
+          if (calculonew == ""){
+            calculonew  = document.getElementById('display').innerText;
+            total = Number(calculo) - Number(calculonew);
+            document.getElementById('display').innerText = total;
+          }else {
+            total = total -  Number(calculonew);
+            document.getElementById('display').innerText = total;
+          }
+            igual = true;
+      }else if (operacion == "*") {
+          if (calculonew == ""){
+            calculonew  = document.getElementById('display').innerText;
+            total = Number(calculo) * Number(calculonew);
+            document.getElementById('display').innerText = total;
+          }else {
+            total = total *  Number(calculonew);
+            document.getElementById('display').innerText = total;
+          }
+            igual = true;
+      }else if (operacion == "/") {
+          if (calculonew == ""){
+            calculonew  = document.getElementById('display').innerText;
+            total = Number(calculo) / Number(calculonew);
+            document.getElementById('display').innerText = total;
+          }else {
+            total = total /  Number(calculonew);
+            document.getElementById('display').innerText = total;
+          }
+            igual = true;
+      }
+
+      if (document.getElementById('display').innerText.length > 8 ){
+        var cadena = document.getElementById('display').innerText;
+        document.getElementById('display').innerText = cadena.substr(0, 8);
       }
     }
   }
 });
-document.getElementById('mas').addEventListener('click', function(){
+function datoscalculadora(oper){
   if (document.getElementById('display').innerText != ""){
-    operacion = "+";
+    operacion = oper;
     calculonew = "";
     calculo = document.getElementById('display').innerText;
     document.getElementById('display').innerText = '';
+  }
+   if (igual == true){
+     igual = false;
+   }
+}
+document.getElementById('mas').addEventListener('click', function(){
+  datoscalculadora("+");
+});
+
+document.getElementById('menos').addEventListener('click', function(){
+  datoscalculadora("-");
+});
+
+document.getElementById('por').addEventListener('click', function(){
+  datoscalculadora("*");
+});
+document.getElementById('dividido').addEventListener('click', function(){
+  datoscalculadora("/");
+});
+
+document.getElementById('sign').addEventListener('click', function(){
+  if (document.getElementById('display').innerText != ""){
+      document.getElementById('display').innerText = (-1) * Number(document.getElementById('display').innerText);
+  }
+});
+
+document.getElementById('punto').addEventListener('click', function(){
+  if (document.getElementById('display').innerText.length < 8){
+    if (document.getElementById('display').innerText == "0"){
+        document.getElementById('display').innerText = document.getElementById('display').innerText + ".";
+    }else{
+      var buscar = document.getElementById('display').innerText;
+      if (buscar.indexOf(".") == -1){
+        document.getElementById('display').innerText = document.getElementById('display').innerText + ".";
+      }
+    }
   }
 });
